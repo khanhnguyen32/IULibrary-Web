dde6b89a9ad424370125c5ffa35d43b3a1519a1f
Nguyen Quoc Khanh <khanhng.32@gmail.com>
2021-05-11 15:46:03 +0700

    setup & UI draft

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 let express = require('express');
 let app = express();
 
+//Set Public Static Folder
+app.use(express.static(__dirname + '/public'));
+
+//Use View Engine
+let expressHbs = require('express-handlebars');
+let hbs = expressHbs.create({
+    ext: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials'
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+//Define routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 //Set Server Port & Start Server
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), () => {
