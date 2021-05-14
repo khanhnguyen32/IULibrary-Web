8950dc2b4c4ae25151c98d11d7bc1eed98273f3e
Mai Oanh <63551651+maioanh2602@users.noreply.github.com>
2021-05-15 00:10:44 +0700

    Update 20210514074726-demo-category.js

--- seeders/20210514074726-demo-category.js
+++ seeders/20210514074726-demo-category.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    let data = [{ name: "Business", imagePath: '/img/home/business.png'},
-      { name: "Computers & Tech", imagePath: 'img/home/Computer_Tech.jpg'},
-      { name: "History", imagePath: 'img/home/History.jpg'},
-      { name: "Science & Math", imagePath: 'img/home/Science_Math.jpg'},
-      { name: "Sport", imagePath: 'img/home/Sport.jpg'}
+  up: (queryInterface, Sequelize) => {
+    let data = [{ name: "Business", imagePath: '/img/home/hero-slide1.png'},
+      { name: "Computers & Tech", imagePath: 'img/home/hero-slide2.png'},
+      { name: "History", imagePath: 'img/home/hero-slide3.png'},
+      { name: "Science & Math", imagePath: 'img/home/hero-slide1.png'},
+      { name: "Sport", imagePath: 'img/home/hero-slide2.png'}
     ];
     
     data.map(item => {
@@ -19,7 +19,7 @@ module.exports = {
    
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Categories', null, {});
     
   }
