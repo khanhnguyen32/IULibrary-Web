3efd4c8cd7f0546e8c1649bce341a047ca214fef
Mai Oanh <63551651+maioanh2602@users.noreply.github.com>
2021-05-15 00:11:55 +0700

    Update 20210514074726-demo-category.js

--- seeders/20210514074726-demo-category.js
+++ seeders/20210514074726-demo-category.js
@@ -2,11 +2,11 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    let data = [{ name: "Business", imagePath: '/img/home/hero-slide1.png'},
-      { name: "Computers & Tech", imagePath: 'img/home/hero-slide2.png'},
-      { name: "History", imagePath: 'img/home/hero-slide3.png'},
-      { name: "Science & Math", imagePath: 'img/home/hero-slide1.png'},
-      { name: "Sport", imagePath: 'img/home/hero-slide2.png'}
+    let data = [{ name: "Business", summary: '', imagePath: '/img/home/hero-slide1.png'},
+      { name: "Computers & Tech", summary: '', imagePath: 'img/home/hero-slide2.png'},
+      { name: "History", summary: '', imagePath: 'img/home/hero-slide3.png'},
+      { name: "Science & Math", summary: '', imagePath: 'img/home/hero-slide1.png'},
+      { name: "Sport", summary: '', imagePath: 'img/home/hero-slide2.png'}
     ];
     
     data.map(item => {
