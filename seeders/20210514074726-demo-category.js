c3a79cdec63e088fad8874a38359f12fab8a9f78
Mai Oanh <63551651+maioanh2602@users.noreply.github.com>
2021-05-14 19:36:08 +0700

    input categories database

--- seeders/20210514074726-demo-category.js
+++ seeders/20210514074726-demo-category.js
@@ -2,23 +2,25 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    let data = [{ name: "Business", imagePath: '/img/home/business.png'},
+      { name: "Computers & Tech", imagePath: 'img/home/Computer_Tech.jpg'},
+      { name: "History", imagePath: 'img/home/History.jpg'},
+      { name: "Science & Math", imagePath: 'img/home/Science_Math.jpg'},
+      { name: "Sport", imagePath: 'img/home/Sport.jpg'}
+    ];
+    
+    data.map(item => {
+      item.createdAt = Sequelize.literal('NOW()');
+      item.updatedAt = Sequelize.literal('NOW()');
+      return item;
+    });
+
+    return queryInterface.bulkInsert('Categories', data, {});
+   
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('Categories', null, {});
+    
   }
 };
