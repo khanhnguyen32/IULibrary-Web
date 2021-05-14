b8a479f69a5e83df3c56741df2c847ae49777f4f
Mai Oanh <63551651+maioanh2602@users.noreply.github.com>
2021-05-14 23:23:41 +0700

    Revert "Merge branch 'Oanh' into main"
    This reverts commit fbbbfb42a5e1a75eb4cb3bf4c3dabf3496daf225, reversing
    changes made to c34932f4efe604c333dccd0c8f23053cf4a7dc34.

--- seeders/20210514074726-demo-category.js
+++ seeders/20210514074726-demo-category.js
@@ -1,25 +1,24 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    let data = [{ name: "Business", summary: "Help more Business Knowledge",imagePath: '/img/home/business.png'},
-      { name: "Computers & Tech", summary: "Help more Technology Knowledge",imagePath: 'img/home/Computer_Tech.jpg'},
-      { name: "History", summary: "Help more Knowledge about History", imagePath: 'img/home/History.jpg'},
-      { name: "Science",  summary: "Help more Knowledge of Science",imagePath: 'img/home/Science_Math.jpg'},
-      { name: "Sport",  summary: "Help more Sport Knowledge",imagePath: 'img/home/Sport.jpg'}
-    ];
-
-    data.map(item => {
-      item.createdAt = Sequelize.literal('NOW()');
-      item.updatedAt = Sequelize.literal('NOW()');
-      return item;
-    });
-
-    return queryInterface.bulkInsert('Categories', data, {});
-
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
   },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Categories', null, {});
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
   }
 };
