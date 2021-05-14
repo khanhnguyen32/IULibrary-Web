8244fa083f3c915ef15bf1d984df5e2c546c57ca
Mai Oanh <63551651+maioanh2602@users.noreply.github.com>
2021-05-14 22:38:20 +0700

    update database

--- seeders/20210514074726-demo-category.js
+++ seeders/20210514074726-demo-category.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    let data = [{ name: "Business", imagePath: '/img/home/business.png'},
-      { name: "Computers & Tech", imagePath: 'img/home/Computer_Tech.jpg'},
-      { name: "History", imagePath: 'img/home/History.jpg'},
-      { name: "Science & Math", imagePath: 'img/home/Science_Math.jpg'},
-      { name: "Sport", imagePath: 'img/home/Sport.jpg'}
+  up: (queryInterface, Sequelize) => {
+    let data = [{ name: "Business", summary: "Help more Business Knowledge",imagePath: '/img/home/business.png'},
+      { name: "Computers & Tech", summary: "Help more Technology Knowledge",imagePath: 'img/home/Computer_Tech.jpg'},
+      { name: "History", summary: "Help more Knowledge about History", imagePath: 'img/home/History.jpg'},
+      { name: "Science",  summary: "Help more Knowledge of Science",imagePath: 'img/home/Science_Math.jpg'},
+      { name: "Sport",  summary: "Help more Sport Knowledge",imagePath: 'img/home/Sport.jpg'}
     ];
-    
+
     data.map(item => {
       item.createdAt = Sequelize.literal('NOW()');
       item.updatedAt = Sequelize.literal('NOW()');
@@ -16,11 +16,10 @@ module.exports = {
     });
 
     return queryInterface.bulkInsert('Categories', data, {});
-   
+
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Categories', null, {});
-    
   }
 };
