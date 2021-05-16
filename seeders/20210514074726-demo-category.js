1b9cb6af2c42e78819178f1724ea96f2eb2ad893
Nguyen Quoc Khanh <khanhng.32@gmail.com>
2021-05-16 15:57:54 +0700

    Update 20210514074726-demo-category.js

--- seeders/20210514074726-demo-category.js
+++ seeders/20210514074726-demo-category.js
@@ -1,25 +1,25 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    let data = [{ name: "Business", summary: "", imagePath: '/img/home/hero-slide1.png'},
-      { name: "Computers & Tech", summary: "", imagePath: '/img/home/hero-slide2.png'},
-      { name: "History", summary: "", imagePath: '/img/home/hero-slide3.png'},
-      { name: "Science & Math", summary: "", imagePath: '/img/home/hero-slide1.png'},
-      { name: "Sport", summary: "", imagePath: '/img/home/hero-slide2.png'}
-  ];
+    up: (queryInterface, Sequelize) => {
+        let data = [{ name: "Business", summary: "", imagePath: '/img/home/hero-slide1.png' },
+            { name: "Computers & Tech", summary: "", imagePath: '/img/home/hero-slide2.png' },
+            { name: "History", summary: "", imagePath: '/img/home/hero-slide3.png' },
+            { name: "Science & Math", summary: "", imagePath: '/img/home/hero-slide1.png' },
+            { name: "Sport", summary: "", imagePath: '/img/home/hero-slide2.png' }
+        ];
 
-    data.map(item => {
-      item.createdAt = Sequelize.literal('NOW()');
-      item.updatedAt = Sequelize.literal('NOW()');
-      return item;
-    });
+        data.map(item => {
+            item.createdAt = Sequelize.literal('NOW()');
+            item.updatedAt = Sequelize.literal('NOW()');
+            return item;
+        });
 
-    return queryInterface.bulkInsert('Categories', data, {});
+        return queryInterface.bulkInsert('Categories', data, {});
 
-  },
+    },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Categories', null, {});
-  }
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('Categories', null, {});
+    }
 };
