f6a736104ed453274e6798350e03db8859735662
Nguyen Quoc Khanh <khanhng.32@gmail.com>
2021-06-06 04:39:08 +0700

    db

--- seeders/04-demo-book.js
+++ seeders/04-demo-book.js
@@ -2,17 +2,36 @@
 
 module.exports = {
     up: async(queryInterface, Sequelize) => {
-        let data = [
-            { address: "01.001", name: "textbook", author: "test", imagePath: '/img/home/hero-slide1.png', thumbnailPath: "", availableQuantity: 1, summary: "test", description: "test", overallReview: 0, reviewCount: 0, majorId: 1, publisherId: 1, categoryId: 2 },
-            { address: "01.001", name: "textbook", author: "test", imagePath: '/img/home/hero-slide1.png', thumbnailPath: "", availableQuantity: 1, summary: "test", description: "test", overallReview: 0, reviewCount: 0, majorId: 1, publisherId: 1, categoryId: 2 },
-            { address: "01.001", name: "textbook", author: "test", imagePath: '/img/home/hero-slide1.png', thumbnailPath: "", availableQuantity: 1, summary: "test", description: "test", overallReview: 0, reviewCount: 0, majorId: 1, publisherId: 1, categoryId: 2 },
-            { address: "01.001", name: "textbook", author: "test", imagePath: '/img/home/hero-slide1.png', thumbnailPath: "", availableQuantity: 1, summary: "test", description: "test", overallReview: 0, reviewCount: 0, majorId: 1, publisherId: 1, categoryId: 2 },
-            { address: "01.001", name: "textbook", author: "test", imagePath: '/img/home/hero-slide1.png', thumbnailPath: "", availableQuantity: 1, summary: "test", description: "test", overallReview: 0, reviewCount: 0, majorId: 2, publisherId: 2, categoryId: 1 },
-            { address: "01.001", name: "textbook", author: "test", imagePath: '/img/home/hero-slide1.png', thumbnailPath: "", availableQuantity: 1, summary: "test", description: "test", overallReview: 0, reviewCount: 0, majorId: 2, publisherId: 2, categoryId: 1 },
-            { address: "01.001", name: "textbook", author: "test", imagePath: '/img/home/hero-slide1.png', thumbnailPath: "", availableQuantity: 1, summary: "test", description: "test", overallReview: 0, reviewCount: 0, majorId: 3, publisherId: 1, categoryId: 1 },
-            { address: "01.001", name: "textbook", author: "test", imagePath: '/img/home/hero-slide1.png', thumbnailPath: "", availableQuantity: 1, summary: "test", description: "test", overallReview: 0, reviewCount: 0, majorId: 4, publisherId: 1, categoryId: 1 },
-            { address: "01.001", name: "textbook", author: "test", imagePath: '/img/home/hero-slide1.png', thumbnailPath: "", availableQuantity: 1, summary: "test", description: "test", overallReview: 0, reviewCount: 0, majorId: 5, publisherId: 1, categoryId: 1 },
-            { address: "01.001", name: "textbook", author: "test", imagePath: '/img/home/hero-slide1.png', thumbnailPath: "", availableQuantity: 1, summary: "test", description: "test", overallReview: 0, reviewCount: 0, majorId: 5, publisherId: 1, categoryId: 1 }
+        let data = [{
+                address: "01.001",
+                name: "textbook",
+                author: "test",
+                imagePath: '/img/home/hero-slide1.png',
+                thumbnailPath: "",
+                availableQuantity: 1,
+                summary: "test",
+                description: "test",
+                overallReview: 0,
+                reviewCount: 0,
+                majorId: 1,
+                publisherId: 1,
+                categoryId: 2
+            },
+            {
+                address: "01.001",
+                name: "textbook",
+                author: "test",
+                imagePath: '/img/home/hero-slide1.png',
+                thumbnailPath: "",
+                availableQuantity: 1,
+                summary: "test",
+                description: "test",
+                overallReview: 0,
+                reviewCount: 0,
+                majorId: 1,
+                publisherId: 1,
+                categoryId: 2
+            },
         ];
 
         data.map(item => {
