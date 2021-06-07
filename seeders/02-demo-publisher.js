c442bba3222aa8f5417c8f73f5144859c1d783d2
Mai Oanh <63551651+maioanh2602@users.noreply.github.com>
2021-06-07 13:24:18 +0700

    Update Database

--- seeders/02-demo-publisher.js
+++ seeders/02-demo-publisher.js
@@ -3,18 +3,21 @@
 module.exports = {
     up: async(queryInterface, Sequelize) => {
         let data = [
-            { name: "Deitel", summary: "", imagePath: '/img/product/B1.jpg' },
-            { name: "CreateSpace Independent Publishing Platform", summary: "", imagePath: '/img/home/hero-slide1.png' },
-            { name: "Bloomberg", summary: "", imagePath: '/img/home/hero-slide1.png' },
-            { name: "Oxford", summary: "", imagePath: '/img/home/hero-slide1.png' },
-            { name: "Cambridge", summary: "", imagePath: '/img/home/hero-slide1.png' },
-            { name: "California", summary: "", imagePath: '/img/home/hero-slide1.png' },
-            { name: "The MIT Press", summary: "", imagePath: '/img/home/hero-slide1.png' },
-            { name: "Cengage Learning", summary: "", imagePath: '/img/home/hero-slide1.png' },
-            { name: "Elsevier", summary: "", imagePath: '/img/home/hero-slide1.png' },
-            { name: "ForDummies", summary: "", imagePath: '/img/home/hero-slide1.png' },
-            { name: "Wrox", summary: "", imagePath: '/img/home/hero-slide1.png' },
-            { name: "Apress", summary: "", imagePath: '/img/home/hero-slide1.png' }
+            { name: "Deitel", summary: "", imagePath: '/img/publisher/Pub1.jpg' },
+            { name: "CreateSpace Independent Publishing Platform", summary: "", imagePath: '/img/publisher/CreateSpace.png' },
+            { name: "Bloomberg", summary: "", imagePath: '/img/publisher/Bloomberg.png' },
+            { name: "Oxford", summary: "", imagePath: '/img/publisher/Oxford.jpg' },
+            { name: "Cambridge", summary: "", imagePath: '/img/publisher/Cambridge.png' },
+            { name: "California", summary: "", imagePath: '/img/publisher/California.png' },
+            { name: "The MIT Press", summary: "", imagePath: '/img/publisher/TheMITPress.png' },
+            { name: "Cengage Learning", summary: "", imagePath: '/img/publisher/Cengage.jpg' },
+            { name: "Elsevier", summary: "", imagePath: '/img/publisher/Elsevier.png' },
+            { name: "ForDummies", summary: "", imagePath: '/img/publisher/ForDummies.jpg' },
+            { name: "Wrox", summary: "", imagePath: '/img/publisher/Wrox.jpg' },
+            { name: "Apress", summary: "", imagePath: '/img/publisher/Apress.jpg' },
+            { name: "Alpha Science", summary: "", imagePath: '/img/publisher/AlphaScience.png' },
+            { name: "De Gruyter Mouton", summary: "", imagePath: '/img/publisher/De.png' },
+            { name: "Kodansha", summary: "", imagePath: '/img/publisher/Kodansha.png' }
         ];
 
         data.map(item => {
