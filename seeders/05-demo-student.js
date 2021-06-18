fbe6fb99d95248a8335b259aabcdd396743d63e6
Nguyen Quoc Khanh <khanhng.32@gmail.com>
2021-06-18 15:27:28 +0700

    update account

--- seeders/05-demo-student.js
+++ seeders/05-demo-student.js
@@ -3,9 +3,9 @@
 module.exports = {
     up: (queryInterface, Sequelize) => {
         let data = [
-            { studentID: "ITITIU18234", password: "123", fullname: "Nguyen Ngoc Mai Oanh", avatarPath: '../img/user/maioanhcute.jpg', isAdmin: false },
-            { studentID: "ITITIU18186", password: "123", fullname: "Nguyen Quoc Khanh", avatarPath: '../img/user/hellokatun.jpg', isAdmin: false },
-            { studentID: "ITITIU18217", password: "123", fullname: "Tran Quoc Thinh", avatarPath: '../img/user/thinhtran.jpg', isAdmin: false },
+            { studentID: "ITITIU18234", password: "$2a$10$NGlAfA.3pC4BX9vg.Lu7a.xQLhQQrvCYHXgbdC0XT1lPnCoQ42xaG", fullname: "Nguyen Ngoc Mai Oanh", avatarPath: '../img/user/maioanhcute.jpg', isAdmin: false },
+            { studentID: "ITITIU18186", password: "$2a$10$NGlAfA.3pC4BX9vg.Lu7a.xQLhQQrvCYHXgbdC0XT1lPnCoQ42xaG", fullname: "Nguyen Quoc Khanh", avatarPath: '../img/user/hellokatun.jpg', isAdmin: false },
+            { studentID: "ITITIU18217", password: "$2a$10$NGlAfA.3pC4BX9vg.Lu7a.xQLhQQrvCYHXgbdC0XT1lPnCoQ42xaG", fullname: "Tran Quoc Thinh", avatarPath: '../img/user/thinhtran.jpg', isAdmin: false },
         ];
 
         data.map(item => {
